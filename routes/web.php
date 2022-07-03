32d95ac6c3a47805704da628d98111fc1c895bf9
LpxsBr <anselmolopes.an@gmail.com>
2022-07-02 21:50:45 -0300

    add initial routes

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/inicio', 'indexController@index')->name('inicio');
+Route::get('/quem-somos', 'quemSomosController@quemsomos');
+Route::get('/plataforma', 'plataformaController@plataforma')->name('plataforma');
+
+Route::get('/', function () {
+    return redirect()->route('inicio');
+});
+
+// fallback to when not exist route
+Route::fallback(function(){
+    echo 'a rota acessada não existe. <a href="'. route('inicio') .'">clique aqui</a> para retornar a homepage.  ';
+})->name('error 404');
